706b49f85bcf48af963ce780b41b3792ed03c1d5
Beiufin <Beiufin@gmail.com>
2024-03-04 15:59:56 -0800

    fix cycle key colors for alt vs

--- keyboards/drop/alt/keymaps/beiufin/keymap.c
+++ keyboards/drop/alt/keymaps/beiufin/keymap.c
@@ -112,7 +112,7 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   _______, AZURE,
         _______, GOLD,    CUSTHSV, GOLD,    CUSTHSV, CUSTHSV, _______, MAGENT,  BLUE,    WHITE,   GREEN,   BLUE,    ORANGE,  _______, ORANGE,
         TRHSV,   CUSTHSV, CUSTHSV, CUSTHSV, CUSTHSV, CUSTHSV, _______, RED,     CYAN,    PURPLE,  _______, _______,          KLHSV,   AZURE,
-        _______, TEAL,    _______, _______, _______, RED,     _______, YELLOW,  GREEN,   CREAM,   _______, _______,          GREEN,   AZURE,
+        _______, TEAL,    TEAL,    TEAL,    TEAL,    RED,     _______, YELLOW,  GREEN,   CREAM,   _______, _______,          GREEN,   AZURE,
         _______, _______, _______,                            _______,                            _______, _______, YELLOW,  RED,     YELLOW,
         // UnderGlow
         CHART,   CHART,   CHART,   CHART,   CHART,   CHART,   CHART,   CHART,   CHART,   CHART,   CHART,   CHART,   CHART,   CHART,   CHART,
